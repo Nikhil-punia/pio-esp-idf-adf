adace78f983718a2d55ab942548e88c98ae7c49d
Nikhil <145488473+Nikhil-punia@users.noreply.github.com>
2025-07-29 09:40:39 +0000

    updated

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-/* Play an MP3, AAC or WAV file from HTTP
+/* Play an MP3 file from HTTP
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -14,18 +14,18 @@
 #include "esp_log.h"
 #include "esp_wifi.h"
 #include "nvs_flash.h"
+#include "sdkconfig.h"
 #include "audio_element.h"
 #include "audio_pipeline.h"
 #include "audio_event_iface.h"
 #include "audio_common.h"
 #include "http_stream.h"
 #include "i2s_stream.h"
+#include "mp3_decoder.h"
 
 #include "esp_peripherals.h"
 #include "periph_wifi.h"
 #include "board.h"
-
-#include "audio_idf_version.h"
 
 #if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0))
 #include "esp_netif.h"
@@ -33,45 +33,7 @@
 #include "tcpip_adapter.h"
 #endif
 
-#define SELECT_AAC_DECODER 1
-
-#if defined SELECT_AAC_DECODER
-#include "aac_decoder.h"
-static const char *TAG = "HTTP_SELECT_AAC_EXAMPLE";
-static const char *selected_decoder_name = "aac";
-static const char *selected_file_to_play = "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.aac";
-#elif defined SELECT_AMR_DECODER
-#include "amr_decoder.h"
-static const char *TAG = "HTTP_SELECT_AMR_EXAMPLE";
-static const char *selected_decoder_name = "amr";
-static const char *selected_file_to_play = "https://dl.espressif.com/dl/audio/ff-16b-1c-8000hz.amr";
-#elif defined SELECT_FLAC_DECODER
-#include "flac_decoder.h"
-static const char *TAG = "HTTP_SELECT_FLAC_EXAMPLE";
-static const char *selected_decoder_name = "flac";
-static const char *selected_file_to_play = "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.flac";
-#elif defined SELECT_MP3_DECODER
-#include "mp3_decoder.h"
-static const char *TAG = "HTTP_SELECT_MP3_EXAMPLE";
-static const char *selected_decoder_name = "mp3";
-static const char *selected_file_to_play = "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.mp3";
-#elif defined SELECT_OGG_DECODER
-#include "ogg_decoder.h"
-static const char *TAG = "HTTP_SELECT_OGG_EXAMPLE";
-static const char *selected_decoder_name = "ogg";
-static const char *selected_file_to_play = "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.ogg";
-#elif defined SELECT_OPUS_DECODER
-#include "opus_decoder.h"
-static const char *TAG = "HTTP_SELECT_OPUS_EXAMPLE";
-static const char *selected_decoder_name = "opus";
-static const char *selected_file_to_play = "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.opus";
-#else
-#include "wav_decoder.h"
-static const char *TAG = "HTTP_SELECT_WAV_EXAMPLE";
-static const char *selected_decoder_name = "wav";
-static const char *selected_file_to_play = "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.wav";
-#endif
-
+static const char *TAG = "HTTP_MP3_EXAMPLE";
 
 void app_main(void)
 {
@@ -89,14 +51,13 @@ void app_main(void)
 #endif
 
     audio_pipeline_handle_t pipeline;
-    audio_element_handle_t http_stream_reader, i2s_stream_writer, selected_decoder;
+    audio_element_handle_t http_stream_reader, i2s_stream_writer, mp3_decoder;
 
     esp_log_level_set("*", ESP_LOG_WARN);
     esp_log_level_set(TAG, ESP_LOG_DEBUG);
 
     ESP_LOGI(TAG, "[ 1 ] Start audio codec chip");
     audio_board_handle_t board_handle = audio_board_init();
-
 
     ESP_LOGI(TAG, "[2.0] Create audio pipeline for playback");
     audio_pipeline_cfg_t pipeline_cfg = DEFAULT_AUDIO_PIPELINE_CONFIG();
@@ -105,52 +66,34 @@ void app_main(void)
 
     ESP_LOGI(TAG, "[2.1] Create http stream to read data");
     http_stream_cfg_t http_cfg = HTTP_STREAM_CFG_DEFAULT();
-    http_cfg.out_rb_size = 1024 * 1024;
+    http_cfg.out_rb_size = 64 * 1024;  // 64KB buffer
+    http_cfg.event_handle = NULL;
+    http_cfg.user_data = NULL;
+    http_cfg.enable_playlist_parser = false;
+    // Disable SSL verification for demo purposes
+    http_cfg.cert_pem = NULL;
     http_stream_reader = http_stream_init(&http_cfg);
 
-    ESP_LOGI(TAG, "[2.2] Create %s decoder to decode %s file", selected_decoder_name, selected_decoder_name);
-
-#if defined SELECT_AAC_DECODER
-    aac_decoder_cfg_t aac_cfg = DEFAULT_AAC_DECODER_CONFIG();
-    selected_decoder = aac_decoder_init(&aac_cfg);
-#elif defined SELECT_AMR_DECODER
-    amr_decoder_cfg_t amr_cfg = DEFAULT_AMR_DECODER_CONFIG();
-    selected_decoder = amr_decoder_init(&amr_cfg);
-#elif defined SELECT_FLAC_DECODER
-    flac_decoder_cfg_t flac_cfg = DEFAULT_FLAC_DECODER_CONFIG();
-    flac_cfg.out_rb_size = 500 * 1024;
-    selected_decoder = flac_decoder_init(&flac_cfg);
-#elif defined SELECT_MP3_DECODER
-    mp3_decoder_cfg_t mp3_cfg = DEFAULT_MP3_DECODER_CONFIG();
-    selected_decoder = mp3_decoder_init(&mp3_cfg);
-#elif defined SELECT_OGG_DECODER
-    ogg_decoder_cfg_t ogg_cfg = DEFAULT_OGG_DECODER_CONFIG();
-    selected_decoder = ogg_decoder_init(&ogg_cfg);
-#elif defined SELECT_OPUS_DECODER
-    opus_decoder_cfg_t opus_cfg = DEFAULT_OPUS_DECODER_CONFIG();
-    selected_decoder = decoder_opus_init(&opus_cfg);
-#else
-    wav_decoder_cfg_t wav_cfg = DEFAULT_WAV_DECODER_CONFIG();
-    selected_decoder = wav_decoder_init(&wav_cfg);
-#endif
-
-    ESP_LOGI(TAG, "[2.3] Create i2s stream to write data to codec chip");
+    ESP_LOGI(TAG, "[2.2] Create i2s stream to write data to codec chip");
     i2s_stream_cfg_t i2s_cfg = I2S_STREAM_CFG_DEFAULT();
     i2s_cfg.type = AUDIO_STREAM_WRITER;
     i2s_stream_writer = i2s_stream_init(&i2s_cfg);
 
+    ESP_LOGI(TAG, "[2.3] Create mp3 decoder to decode mp3 file");
+    mp3_decoder_cfg_t mp3_cfg = DEFAULT_MP3_DECODER_CONFIG();
+    mp3_decoder = mp3_decoder_init(&mp3_cfg);
+
     ESP_LOGI(TAG, "[2.4] Register all elements to audio pipeline");
     audio_pipeline_register(pipeline, http_stream_reader, "http");
-    audio_pipeline_register(pipeline, selected_decoder,    selected_decoder_name);
+    audio_pipeline_register(pipeline, mp3_decoder,        "mp3");
     audio_pipeline_register(pipeline, i2s_stream_writer,  "i2s");
 
-    ESP_LOGI(TAG, "[2.5] Link it together http_stream-->%s_decoder-->i2s_stream-->[codec_chip]", selected_decoder_name);
-    const char *link_tag[3] = {"http", selected_decoder_name, "i2s"};
+    ESP_LOGI(TAG, "[2.5] Link it together http_stream-->mp3_decoder-->i2s_stream-->[codec_chip]");
+    const char *link_tag[3] = {"http", "mp3", "i2s"};
     audio_pipeline_link(pipeline, &link_tag[0], 3);
 
-    ESP_LOGI(TAG, "[2.6] Set up  uri (http as http_stream, %s as %s_decoder, and default output is i2s)",
-             selected_decoder_name, selected_decoder_name);
-    audio_element_set_uri(http_stream_reader, selected_file_to_play);
+    ESP_LOGI(TAG, "[2.6] Set up  uri (http as http_stream, mp3 as mp3 decoder, and default output is i2s)");
+    audio_element_set_uri(http_stream_reader, "https://stream.radioparadise.com/global-320");
 
     ESP_LOGI(TAG, "[ 3 ] Start and wait for Wi-Fi network");
     esp_periph_config_t periph_cfg = DEFAULT_ESP_PERIPH_SET_CONFIG();
@@ -162,7 +105,8 @@ void app_main(void)
     esp_periph_handle_t wifi_handle = periph_wifi_init(&wifi_cfg);
     esp_periph_start(set, wifi_handle);
     periph_wifi_wait_for_connected(wifi_handle, portMAX_DELAY);
-
+    
+    // Example of using an audio event -- START
     ESP_LOGI(TAG, "[ 4 ] Set up  event listener");
     audio_event_iface_cfg_t evt_cfg = AUDIO_EVENT_IFACE_DEFAULT_CFG();
     audio_event_iface_handle_t evt = audio_event_iface_init(&evt_cfg);
@@ -185,13 +129,13 @@ void app_main(void)
         }
 
         if (msg.source_type == AUDIO_ELEMENT_TYPE_ELEMENT
-            && msg.source == (void *) selected_decoder
+            && msg.source == (void *) mp3_decoder
             && msg.cmd == AEL_MSG_CMD_REPORT_MUSIC_INFO) {
             audio_element_info_t music_info = {0};
-            audio_element_getinfo(selected_decoder, &music_info);
+            audio_element_getinfo(mp3_decoder, &music_info);
 
-            ESP_LOGI(TAG, "[ * ] Receive music info from %s decoder, sample_rates=%d, bits=%d, ch=%d",
-                     selected_decoder_name, music_info.sample_rates, music_info.bits, music_info.channels);
+            ESP_LOGI(TAG, "[ * ] Receive music info from mp3 decoder, sample_rates=%d, bits=%d, ch=%d",
+                     music_info.sample_rates, music_info.bits, music_info.channels);
 
             i2s_stream_set_clk(i2s_stream_writer, music_info.sample_rates, music_info.bits, music_info.channels);
             continue;
@@ -205,16 +149,18 @@ void app_main(void)
             break;
         }
     }
+    // Example of using an audio event -- END
 
-    ESP_LOGI(TAG, "[ 6 ] Stop audio_pipeline and release all resources");
+    ESP_LOGI(TAG, "[ 6 ] Stop audio_pipeline");
     audio_pipeline_stop(pipeline);
     audio_pipeline_wait_for_stop(pipeline);
     audio_pipeline_terminate(pipeline);
-    audio_pipeline_unregister(pipeline, http_stream_reader);
-    audio_pipeline_unregister(pipeline, i2s_stream_writer);
-    audio_pipeline_unregister(pipeline, selected_decoder);
 
     /* Terminate the pipeline before removing the listener */
+    audio_pipeline_unregister(pipeline, http_stream_reader);
+    audio_pipeline_unregister(pipeline, i2s_stream_writer);
+    audio_pipeline_unregister(pipeline, mp3_decoder);
+
     audio_pipeline_remove_listener(pipeline);
 
     /* Stop all peripherals before removing the listener */
@@ -228,6 +174,6 @@ void app_main(void)
     audio_pipeline_deinit(pipeline);
     audio_element_deinit(http_stream_reader);
     audio_element_deinit(i2s_stream_writer);
-    audio_element_deinit(selected_decoder);
+    audio_element_deinit(mp3_decoder);
     esp_periph_set_destroy(set);
 }
